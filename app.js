54489dadb660bd8873aca1b3892fe7e39b8318f1
sikgyu <57619483+sikgyu@users.noreply.github.com>
2021-03-10 00:19:37 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const fs = require("fs");
+let globalData;
+
+let app = express();
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) => res.render("pages/index"));
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000 🚀");
+  });
