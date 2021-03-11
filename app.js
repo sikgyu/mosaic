4a860668b100b249d1909957e089cd2ee97fe5fc
sikgyu <57619483+sikgyu@users.noreply.github.com>
2021-03-11 01:48:16 -0800

    port number

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("pages/index"));
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000 🚀");
+app.listen(8080, () => {
+    console.log("Server is running on port 8080 🚀");
   });
